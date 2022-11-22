de35234b70958f334295950c75b2d535882f67e1
Rivaldo Stuart Wattimena <rivaldo.wattimena@shopee.com>
2022-11-22 13:43:07 +0700

    refactor: refactor login page and add responsive

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -40,63 +40,75 @@ const Login = () => {
   };
 
   return (
-    <div className="auth-container d-flex flex-column justify-content-center">
-      <h1 className="text-center">Login</h1>
-      <form
-        onSubmit={handleSubmit}
-        className="row flex-column align-items-center gy-4"
-      >
-        <div className="col-lg-3 text-start">
-          <label htmlFor="email" className="fw-bold fs-5 form-label">
-            Email
-          </label>
-          <input
-            className="form-control p-3"
-            type="email"
-            name="email"
-            id="email"
-            placeholder="test@test.com"
-            onChange={handleChange}
-          />
-          {isError.email ? (
-            <div className="text-danger mt-3">Email should not be empty</div>
-          ) : (
-            ""
-          )}
+    <div className="auth-container row gap-5 gap-lg-0">
+      <div className="col-12 col-lg-6 my-auto">
+        <div className="mb-5">
+          <h1 className="text-center">Welcome Back!</h1>
+          <p className="text-center fs-4">We are happy to have you back</p>
         </div>
-        <div className="col-lg-3 text-start">
-          <label htmlFor="password" className="fw-bold fs-5 form-label">
-            Password
-          </label>
-          <input
-            className="form-control p-3"
-            type="password"
-            name="password"
-            id="password"
-            placeholder="******"
-            onChange={handleChange}
-          />
-          {isError.password ? (
-            <div className="text-danger mt-3">Password should not be empty</div>
-          ) : (
-            ""
-          )}
+        <form
+          onSubmit={handleSubmit}
+          className="row flex-column align-items-center gy-4"
+        >
+          <div className="col-lg-5 text-start">
+            <input
+              className="form-control p-3"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+              onChange={handleChange}
+            />
+            {isError.email ? (
+              <div className="text-danger mt-3">Email should not be empty</div>
+            ) : (
+              ""
+            )}
+          </div>
+          <div className="col-lg-5 text-start">
+            <input
+              className="form-control p-3"
+              type="password"
+              name="password"
+              id="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            {isError.password ? (
+              <div className="text-danger mt-3">
+                Password should not be empty
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+          <div className="col-lg-5 text-center mt-5">
+            <button type="submit" className="btn btn-dark w-75">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="col-12 col-lg-6 my-auto p-0">
+        <div className="border border-1 w-75 mx-auto shadow py-5">
+          <div className="row flex-column align-items-center text-center justify-content-center gap-5">
+            <img
+              src="/assets/login.svg"
+              alt="login image"
+              className="img-thumbnail border-0 col-lg-7 col-8"
+            />
+            <div className="col-lg-10 d-flex flex-column gap-3">
+              <h1>Don't have an account?</h1>
+              <span className="fs-4">
+                Get started by creating your new account
+              </span>
+              <Link to="/register" className="mt-3 text-decoration-none">
+                <button className="btn btn-outline-dark w-50">Register</button>
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="col-lg-3">
-          <button
-            type="submit"
-            className="btn btn-info form-control text-white"
-          >
-            Login
-          </button>
-        </div>
-      </form>
-      <p className="text-center">
-        You don't have an account?
-        <Link to="/register" className="ms-2 text-decoration-none">
-          <span>Sign Up</span>
-        </Link>
-      </p>
+      </div>
     </div>
   );
 };
