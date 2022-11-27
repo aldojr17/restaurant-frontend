102a0c7238468b23b9babb68218900d082b0b650
Rivaldo Stuart Wattimena <rivaldo.wattimena@shopee.com>
2022-11-27 16:01:50 +0700

    refactor: filter display

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -43,73 +43,73 @@ const Filter = () => {
   }, [debounce, filter]);
 
   return (
-    <div className="container mx-auto row justify-content-between align-items-center px-5 gap-3 gap-lg-0">
-      <div className="col-lg-4">
-        <div className="row align-items-center gap-2">
-          <span className="col-lg-3">Sort By</span>
-          <select
-            className="form-select col"
-            name="sortBy"
-            id="sortBy"
-            onChange={handleChangeFilter}
-          >
-            <option value="price">Price</option>
-            <option value="name">Name</option>
-            <option value="rating">Rating</option>
-            <option value="is_available">Available</option>
-          </select>
-          <select
-            className="form-select col"
-            name="sort"
-            id="sort"
-            onChange={handleChangeFilter}
-          >
-            <option value="asc">Ascending</option>
-            <option value="desc">Descending</option>
-          </select>
-        </div>
+    <div className="container px-lg-5 row flex-column mx-auto gap-4 align-items-center align-items-lg-start">
+      <div className="col d-flex form-control align-items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="bi bi-search"
+          viewBox="0 0 16 16"
+        >
+          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+        </svg>
+        <input
+          type="search"
+          name="search"
+          id="search"
+          className="border-0 form-control"
+          placeholder="Search Menu Name"
+          value={searchText}
+          onChange={handleSearch}
+        />
       </div>
-      <div className="col-lg-4">
-        <div className="d-flex form-control align-items-center gap-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            fill="currentColor"
-            className="bi bi-search"
-            viewBox="0 0 16 16"
-          >
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
-          </svg>
-          <input
-            type="search"
-            name="search"
-            id="search"
-            className="border-0 form-control"
-            placeholder="Search Menu Name"
-            value={searchText}
-            onChange={handleSearch}
-          />
+      <div className="col row justify-content-between align-items-center gap-3 gap-lg-0 p-0">
+        <div className="col-lg-4">
+          <div className="row align-items-center gap-2">
+            <span className="col-lg-3">Sort By</span>
+            <select
+              className="form-select col"
+              name="sortBy"
+              id="sortBy"
+              onChange={handleChangeFilter}
+            >
+              <option value="price">Price</option>
+              <option value="name">Name</option>
+              <option value="rating">Rating</option>
+              <option value="is_available">Available</option>
+            </select>
+            <select
+              className="form-select col"
+              name="sort"
+              id="sort"
+              onChange={handleChangeFilter}
+            >
+              <option value="asc">Ascending</option>
+              <option value="desc">Descending</option>
+            </select>
+          </div>
         </div>
-      </div>
-      <div className="col-lg-3">
-        <div className="row align-items-center">
-          <span className="col-lg-4">Filter By</span>
-          <select
-            className="form-select col"
-            name="category"
-            id="category"
-            onChange={handleChangeFilter}
-          >
-            <option key={0} value={0}>
-              All
-            </option>
-            {categories.map((category) => (
-              <option key={category.id} value={category.id}>
-                {category.name}
+        <div className="col-lg-3 p-lg-0">
+          <div className="row align-items-center">
+            <span className="col-lg-4">Filter By</span>
+            <select
+              className="form-select col"
+              name="category"
+              id="category"
+              onChange={handleChangeFilter}
+            >
+              <option key={0} value={0}>
+                All
               </option>
-            ))}
-          </select>
+              {categories.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       </div>
     </div>
