b7c61cb529162268f92d99b80eee4c945ab34f47
Rivaldo Stuart Wattimena <rivaldo.wattimena@shopee.com>
2022-11-29 13:44:51 +0700

    fix: fix bug in favorite page

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -1,10 +1,18 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Card from "../components/Card/Card";
 import Navbar from "../components/Navbar/Navbar";
 import { RootState } from "../redux";
+import { getProfile } from "../redux/user/action";
+import { UserDispatch } from "../redux/user/types";
 
 const Favorites = () => {
   const { user } = useSelector((state: RootState) => state.userReducer);
+  const dispatch: UserDispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProfile());
+  }, []);
 
   return (
     <>
